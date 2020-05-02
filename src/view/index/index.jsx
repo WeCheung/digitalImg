2e4b030bd397a4a64a5cccf127b3a2648b094c09
Harold <1003306162@qq.com>
2020-05-02 18:56:11 +0800

    初始化项目

---
+++ src/view/index/index.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './index.scss';
+
+class MainPage extends Component {
+  render() {
+    return (
+        <div id={'main-page'}>
+          <div className="main-box">
+            <div className="btn-group1">
+              
+            </div>
+            <canvas id="my-canvas"/>
+          </div>
+
+        </div>
+    );
+  }
+}
+
+export default MainPage;

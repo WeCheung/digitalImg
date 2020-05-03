53abb2f92553af1574447e30ee0781ad01ca41b5
Harold <1003306162@qq.com>
2020-05-03 10:54:51 +0800

    添加并整理能用上的所有函数，样式上添加了副屏

---
+++ src/view/index/base/secondScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SecondScreen() {
+  return (
+      <div className={'second-screen'}>
+
+      </div>
+  )
+}
+
+export default SecondScreen;
--- src/view/index/index.jsx
+++ src/view/index/index.jsx
@@ -2,13 +2,17 @@ import React, {Component} from 'react';
 import './index.scss';
 import Main from "./base/main";
 import BtnGroup from "./base/btnGroup";
+import SecondScreen from "./base/secondScreen";
 
 class MainPage extends Component {
   render() {
     return (
         <div id={'main-page'}>
           <Main />
-          <BtnGroup />
+          <div className={'bottom-group'}>
+            <BtnGroup />
+            <SecondScreen />
+          </div>
         </div>
     );
   }
